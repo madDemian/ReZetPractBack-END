ccc2c640b84f6a21b0bc8621535dddbb7f387675
madDemian <danielkhorev@gmail.com>
2021-07-29 17:11:56 +0300

    1 commit 2 ex. PostController Models Migrate

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $data = Post::all();
+        return $data;
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        $post = Post::create($request->all());
+
+        return $post;
+    }
+}
