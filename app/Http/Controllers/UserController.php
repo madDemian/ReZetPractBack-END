56443ef99673b9633dee038e403890fa66e793d3
madDemian <danielkhorev@gmail.com>
2021-08-10 11:13:05 +0300

    added filtering by Author

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(User $user){
+        return response()->json($user->posts()->get());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 
 Route::get('posts', [PostController::class, "index"]);
+Route::get('/{user}/posts',[UserController::class,'index']);
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
+
 
 Route::group(['middleware'=>['auth:sanctum']], function (){
     Route::post('/logout',[AuthController::class,'logout']);
