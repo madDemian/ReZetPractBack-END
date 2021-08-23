0abd58de8efcab2314cf2d4835210e1d9fb2d6f7
madDemian <danielkhorev@gmail.com>
2021-08-23 15:06:04 +0300

    reworked UserResource.php.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,7 +17,8 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'first_name' => $this->first_name,
-            'last_name' => $this->last_name
+            'last_name' => $this->last_name,
+            'email' => $this->email
         ];
     }
 }
