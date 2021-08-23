d7534536ef653484dfec2eeaffceebd211e9b428
madDemian <danielkhorev@gmail.com>
2021-08-23 12:40:14 +0300

    reworked UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
 use App\Http\Resources\UserResource;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function getUserPosts(User $user){
-        return UserResource::collection($user->posts()->get());
+        return PostResource::collection(Post::orderBy('created_at', 'desc')->get());
     }
     public function authMe(Request $request){
         return new UserResource($request->user());
